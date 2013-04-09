d3ea89e3739737cd96bd53153d0ad6af9482a78e
Dinanath Sulakhe <sulakhe@gmail.com>
2013-04-09 08:21:26 -0500

    Added Parallel Exporters for GO concurrent transfers

--- source/java/org/rsna/ctp/stdstages/GlobusExportService.java
+++ source/java/org/rsna/ctp/stdstages/GlobusExportService.java
@@ -35,7 +35,23 @@ public class GlobusExportService extends AbstractExportService{
 	String destinationPassword = null;
 	String destinationRoot = null;
 	int transferWaitTime = 3600; //In Seconds
+	int poolSize = 1;
 	
+	Exporter[] exporters = null;
+	int successCount = 0;
+	int retryCount = 0;
+
+	int throttle = 0;
+	
+	static final int defaultInterval = 5000;
+	static final int minInterval = 1000;
+	static final int maxInterval = 2 * defaultInterval;
+
+	static final int minPoolSize = 1;
+	static final int maxPoolSize = 10;
+
+	int interval = defaultInterval;
+
 
 
 	public GlobusExportService(Element element) throws Exception {
@@ -56,6 +72,8 @@ public class GlobusExportService extends AbstractExportService{
 		destinationRoot = element.getAttribute("destinationRoot");
 		destinationUsername = element.getAttribute("destinationUsername");
 		destinationPassword = element.getAttribute("destinationPassword");
+		poolSize = Integer.parseInt(element.getAttribute("maxPoolSize"));
+		
 		if(!element.getAttribute("transferWaitTime").isEmpty())
 		{
 			transferWaitTime = Integer.parseInt(element.getAttribute("transferWaitTime")) * 60;
@@ -69,7 +87,17 @@ public class GlobusExportService extends AbstractExportService{
 	 * Start the export thread.
 	 */
 	public void start() {
-		startExportThread();
+		int queueSize = poolSize;
+		logger.info("Queue Size: " + queueSize);
+		exporters = new Exporter[queueSize];
+		
+		for(int i=0; i<queueSize; i++){
+			exporters[i] = new Exporter();
+			exporters[i].start();
+			logger.info("Thread " + i + " started..");
+		}
+		
+		//startExportThread();
 	}
 
 	@Override
@@ -151,7 +179,9 @@ public class GlobusExportService extends AbstractExportService{
 				
 			}
 			logger.info("File to be transferred: " + sPath);
-
+			logger.info("Canonical Path: " + file.getCanonicalPath());
+			logger.info("Absolute Path: " + file.getAbsolutePath());
+			logger.info("Get Path: " + file.getPath());			
 			
 
 			JSONTransferAPIClient.Result r = client.getResult("/transfer/submission_id");
@@ -191,6 +221,71 @@ public class GlobusExportService extends AbstractExportService{
 		logger.info("Transfer complete..");
 		return Status.OK;
 
+	}
+	
+	
+	class Exporter extends Thread {
+		public Exporter() {
+			super(name + " Exporter");
+			logger.info("New Exporter class..");
+		}
+		public void run() {
+			logger.info(name+": Exporter Thread: Started");
+			File file = null;
+			while (!stop && !interrupted()) {
+				try {
+					if ((getQueueSize()>0) && connect().equals(Status.OK)) {
+						while (!stop && ((file = getNextFile()) != null)) {
+							Status result = export(file);
+							if (result.equals(Status.FAIL)) {
+								//Something is wrong with the file.
+								//Log a warning and quarantine the file.
+								logger.warn(name+": Unable to export "+file);
+								if (quarantine != null) quarantine.insert(file);
+								else file.delete();
+							}
+							else if (result.equals(Status.RETRY)) {
+								//Something is wrong, but probably not with the file.
+								//Note that the file has been removed from the queue,
+								//so it is necessary to requeue it. This has the
+								//effect of moving it to the end of the queue.
+								getQueueManager().enqueue(file);
+								//Note that enqueuing a file does not delete it
+								//from the source location, so we must delete it now.
+								file.delete();
+								logger.debug("Status.RETRY received: successCount = "+successCount+"; retryCount = "+retryCount);
+								successCount = 0;
+								//Only break if we have had a string of failures
+								//in a row; otherwise, move on to the next file.
+								if (retryCount++ > 5) break;
+							}
+							else {
+								if (throttle > 0) {
+									try { Thread.sleep(throttle); }
+									catch (Exception ignore) { }
+								}
+								release(file);
+								successCount++;
+								retryCount = 0;
+							}
+						}
+						disconnect();
+					}
+					if (!stop) sleep(interval);
+					//Recount the queue in case it has been corrupted by
+					//someone copying files into the queue directories by hand.
+					//To keep from doing this when it doesn't really matter and
+					//it might take a long time, only do it when the remaining
+					//queue is small.
+					if (!stop && (getQueueSize() < 20)) recount();
+				}
+				catch (Exception e) {
+					logger.warn(name+" Exporter Thread: Exception received",e);
+					stop = true;
+				}
+			}
+			logger.info(name+" Thread: Interrupt received; thread stopped");
+		}
 	}
 
 
